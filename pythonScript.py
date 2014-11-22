ddd41bb1056552124413086e3e62bc9c107d4f8f
cmenekse <canmenekse@sabanciuniv.edu>
2014-11-22 16:27:53 +0200

    py: Script now compiles files and moves the .exe to the needed directory

--- pythonScript.py
+++ pythonScript.py
@@ -1,40 +1,56 @@
 import os;
 import zipfile;
 import re;
+import subprocess;
+
+compilerDirectory="C:/\"Program Files (x86)\"/\"Microsoft Visual Studio 12.0\"/VC/bin/";
+batFile="vcvars32.bat";
+
 def traverse_all_directories(directory):
     zip_pattern =re.compile('.*\.zip$')
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for filename in files:
-        #print(filename);
         if(zip_pattern.match(filename)):
             print(filename);
             extract_zip(filename,"extracted");
 
+def compileFile(full_path,filename_without_extension,directory):
+
+    compileCommand="cl/EHsc "+ addQuote(full_path);
+    changeDirectoryCommand="cd " + compilerDirectory;
+    combinedCommand = changeDirectoryCommand+"&&" + batFile+"&&"+compileCommand;
+    moveCommand = changeDirectoryCommand + "&&" +"move " + addQuote(filename_without_extension+".exe")+" " + addQuote(directory);
+    os.system(combinedCommand)
+    os.system(moveCommand)
+
+def addQuote(str):
+    return '"'+str+'"'
+
 def traverse_with_os_walk ():
     zip_pattern =re.compile('(.*)\.zip$')
     cpp_pattern =re.compile('(.*)\.cpp$')
-    rootDirectory = os.getcwd();
-    for root, dirs, files in os.walk(rootDirectory, topdown= True):
+    mainStartingDirectory = os.getcwd();
+    for os_walk_root, dirs, files in os.walk(mainStartingDirectory, topdown= True):
         for filename in files:
             zipMatch = re.search(zip_pattern,filename)
             cppMatch = re.search(cpp_pattern,filename)
             if(zipMatch):
                 filename_without_extension=zipMatch.group(1)
-                full_path_of_file=os.path.join(rootDirectory,root,zipMatch.group(0))
-                extract_directory=os.path.join(rootDirectory,root,filename_without_extension)
+                full_path_of_file=os.path.join(mainStartingDirectory,os_walk_root,zipMatch.group(0))
+                extract_directory=os.path.join(mainStartingDirectory,os_walk_root,filename_without_extension)
                 extract_zip(full_path_of_file,extract_directory);
                 dirs.append(filename_without_extension)
             elif(cppMatch):
-                full_path_of_file=os.path.join(rootDirectory,root,cppMatch.group(0))
-                print (full_path_of_file);
+                directory = os.path.join(mainStartingDirectory,os_walk_root)
+                full_path_of_file=os.path.join(mainStartingDirectory,os_walk_root,cppMatch.group(0))
+                filename_without_extension = cppMatch.group(1);
+                compileFile(full_path_of_file,filename_without_extension,directory);
 
-
-        
 def extract_zip (source,destination):
     #print("Extracting "  + source + " to  " + destination)
     with  zipfile.ZipFile(source,'r') as zf:
         zf.extractall(destination);
 
 if __name__ == "__main__":
-	#traverse_all_directories();
     traverse_with_os_walk();
+
